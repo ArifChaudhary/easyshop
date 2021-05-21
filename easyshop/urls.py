534739b19dfc539be0a7889d743a8e2554546c33
ArifChaudhary <arif.chaudhary.geu864@gmail.com>
2021-05-21 16:32:29 +0530

    first commit

---
+++ easyshop/urls.py
@@ -0,0 +1,9 @@
+"""EasyShop URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+]
